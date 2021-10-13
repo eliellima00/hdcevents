9df06d5776885ef5df45572d7eed2978fd55f9d4
ELIEL MATOS DE LIMA <eliel.lima@attosementes.com.br>
2021-10-13 09:13:13 -0400

    Adicionando view do evento, recuperando dados do evento, enviando JSON

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -8,19 +8,22 @@ use App\Models\Event;
 
 class EventController extends Controller
 {
-    public function index() {
+    public function index()
+    {
 
         $events = Event::all();
 
-        return view('Welcome',['events' => $events ]);
+        return view('Welcome', ['events' => $events]);
     }
 
-    public function create () {
+    public function create()
+    {
         return view('events.create');
     }
 
     //Request é do laravel e ja tras todas as informações dos formularios
-    public function store(Request $request) {
+    public function store(Request $request)
+    {
 
         $event = new Event;
 
@@ -28,15 +31,32 @@ class EventController extends Controller
         $event->city = $request->city;
         $event->private = $request->private;
         $event->description = $request->description;
+        $event->items = $request->items; // no model eu digo que items é do tipo array
 
         //Image Upload
-        if($request->hasFile('image') ** request->file('image')->isValid()) {
+        if ($request->hasFile('image') && $request->file('image')->isValid()) {
 
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $requestImage->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
         }
 
         $event->save();
 
         return redirect('/')->with('msg', 'Evento criado com sucesso!');
+    }
+
+    public function show($id) {
+
+        $event = Event::findOrFail($id);
+
+        return view('events.show', ['event' => $event]);
 
     }
 }
